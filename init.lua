8a55f725659c567b88e3ce9b29e04cc5fc87232d
Britt Carter <bcarter@priefert.com>
2023-10-02 17:23:02 -0500

    Cleanup

--- init.lua
+++ init.lua
@@ -132,5 +132,3 @@ vim.keymap.set("v", "<leader>d", "\"_d", { desc = "[D]elete to void register" })
 vim.keymap.set("n", "Q", "<nop>")
 --vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>", { desc = "[S]earch and replace" })
-
--- Color Scheme
