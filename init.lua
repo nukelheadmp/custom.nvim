96014c83d3e02004af547169b67c340a2f2e3fe5
Britt Carter <bcarter@priefert.com>
2023-10-03 16:39:17 -0500

    Added nightfox colorscheme, yank/paste to system clipboard

--- init.lua
+++ init.lua
@@ -16,14 +16,23 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   {
-    "folke/tokyonight.nvim",
+    "EdenEast/nightfox.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
+      vim.cmd("colorscheme nightfox")
     end,
   },
+  --{
+  --  "folke/tokyonight.nvim",
+  --  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --  priority = 1000, -- make sure to load this before all the other start plugins
+  --  config = function()
+  --    -- load the colorscheme here
+  --    vim.cmd([[colorscheme tokyonight]])
+  --  end,
+  --},
   --{
   --  'rebelot/kanagawa.nvim',
   --  lazy = false,
