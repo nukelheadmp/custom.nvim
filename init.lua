09c8395fab4badd629dda69f1126ada6644cd2f7
Britt Carter <bcarter@priefert.com>
2023-10-24 16:59:07 -0500

    Changed theme

--- init.lua
+++ init.lua
@@ -23,23 +23,23 @@ require("lazy").setup({
   --    require("colorizer").setup()
   --  end,
   --},
-  {
-    "EdenEast/nightfox.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd("colorscheme nightfox")
-    end,
-  },
   --{
-  --  "folke/tokyonight.nvim",
-  --  lazy = false,
-  --  priority = 1000,
+  --  "EdenEast/nightfox.nvim",
+  --  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --  priority = 1000, -- make sure to load this before all the other start plugins
   --  config = function()
-  --    vim.cmd([[colorscheme tokyonight]])
+  --    -- load the colorscheme here
+  --    vim.cmd("colorscheme nightfox")
   --  end,
   --},
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
   --{
   --  'rebelot/kanagawa.nvim',
   --  lazy = false,
