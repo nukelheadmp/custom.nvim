490ada4af2c8fa5f3cbb5d20eab46c3a38ea70a5
Britt Carter <nukelheadmp@gmail.com>
2024-03-12 00:06:02 -0500

    Set case insensitive search

--- lua/settings.lua
+++ lua/settings.lua
@@ -17,6 +17,7 @@ vim.opt.undodir = os.getenv('HOME') .. "/.vim/undodir"
 vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.ignorecase = true
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
