f6cffec02df6ba2e45995320bc08d7a98debdeb6
Britt Carter <nukelheadmp@gmail.com>
2024-06-19 01:06:26 -0500

    Set slate colorscheme

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 require("settings")
 require("keymaps")
 
+vim.cmd("colorscheme slate")
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
