53435224a8bca698a4c4fe8c9bc36759d850f396
Britt Carter <nukelheadmp@gmail.com>
2024-08-11 15:40:54 -0500

    Swapped to NeoTree instead of nvim-tree

--- lua/plugins-available/transparent.lua
+++ lua/plugins-available/transparent.lua
@@ -4,6 +4,7 @@ return {
     config = function()
       vim.g.transparent_groups = vim.list_extend(vim.g.transparent_groups or {}, { "ExtraGroup" })
       --require("nightfox").setup{ transparent = vim.g.transparent_enabled }
+      require("transparent").clear_prefix("NeoTree")
     end,
   }
 }
