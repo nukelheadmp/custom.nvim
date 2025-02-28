25d266c0ada238b7c3639ed2a1a1aa6c456d9c19
Britt Carter <bcarter@priefert.com>
2025-02-28 11:07:56 -0600

    Added functionality for deleting buffers

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,5 @@
+local ui = require("util.ui")
+
 -- Keymaps
 -- Remap for dealing with word wrap
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
@@ -14,7 +16,7 @@ vim.keymap.set("n", "<leader>fe", vim.cmd.Ex, { desc = "[F]ile [E]xplorer" })
 vim.keymap.set("n", "<leader>ft", ":Neotree toggle<cr>", { desc = "File [T]ree" })
 --vim.keymap.set("n", "<leader>w", "<C-w>", { desc = "[W]indow operation" })
 vim.keymap.set("n", "<leader>b", "<nop>", { desc = "Buffer Operations" })
-vim.keymap.set("n", "<leader>bd", ":bd<cr>", { desc = "[B]uffer [D]elete" })
+vim.keymap.set("n", "<leader>bd", ui.bufremove, { desc = "[B]uffer [D]elete" })
 vim.keymap.set("n", "<leader>bn", ":bn<cr>", { desc = "[B]uffer [N]ext" })
 vim.keymap.set("n", "<leader>bp", ":bp<cr>", { desc = "[B]uffer [P]revious" })
 
