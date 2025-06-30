60668de2ddee1a42fb3b113dc4b8ab917342b382
Britt Carter <bcarter@priefert.com>
2025-06-30 10:32:31 -0500

    Treesitter changes?

--- lua/plugins-available/treesitter.lua
+++ lua/plugins-available/treesitter.lua
@@ -1,3 +1,12 @@
 return {
   {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  --config = function()
+  --  vim.opt.foldmethod = "expr"
+  --  vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+  --  vim.opt.foldcolumn = "0"
+  --  vim.opt.foldtext = ""
+  --  vim.opt.foldlevel = 99
+  --  vim.opt.foldlevelstart = 1
+  --  vim.opt.foldnestmax = 4
+  --end
 }
